ecfc97064dba3a61c36373b43a76861625f4b470
Steveraaen <sraaen@gmail.com>
2018-11-18 18:18:09 -0500

    made a line

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import * as d3 from 'd3';
 
-console.log('hello')
-
-const square = d3.selectAll("rect");
-square.style("fill", "orange");
+         var width = 300;
+         var height = 300;
+         var svg = d3.select("#svgcontainer")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height);
+         svg.append("line")
+            .attr("x1", 100)
+            .attr("y1", 100)
+            .attr("x2", 200)
+            .attr("y2", 200)
+            .style("stroke", "rgb(255,0,0)")
+            .style("stroke-width", 2);
